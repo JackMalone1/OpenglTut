288ea9d8521ff55a2532c1d07264769156efb82e
Jack Malone <c00236428@itcarlow.ie>
2020-05-06 23:41:08 +0100

    created a cube and a camera that you are able to move around

--- src/EngineTester/MainGameLoop.java
+++ src/EngineTester/MainGameLoop.java
@@ -1,11 +1,14 @@
 package EngineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
 import RenderEngine.DisplayManager;
 import RenderEngine.Loader;
 import RenderEngine.Renderer;
 import Shaders.StaticShader;
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import textures.ModelTexture;
@@ -18,37 +21,104 @@ public class MainGameLoop
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-				    -0.5f, 0.5f, 0f,
-				    -0.5f, -0.5f, 0f,
-				    0.5f, -0.5f, 0f,	   
-				    0.5f, 0.5f, 0f		    
-		};//makes a square on the screen
+		Camera camera = new Camera();
 		
-		int[] indices = {
-				0,1,3,
-				3,1,2
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-			0,0,
-			0,1,
-			1,1,
-			1,0
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("Idle1"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
+		
+		Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5), 0, 0, 0, 1);
 
 		while(!Display.isCloseRequested())
 		{
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.Prepare();
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
